be0efec1123494c4cdef8bdda0dcd5a34e945277
prithwiraj041981 <email_address>
2023-06-24 13:50:07 +0530

    java project2

---
+++ src/test/java/javapoint1/TestJavaCollection6.java
@@ -0,0 +1,19 @@
+package javapoint1;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class TestJavaCollection6 {
+	public static void main(String[] args) {  
+		//Creating Deque and adding elements  
+		Deque<String> deque = new ArrayDeque<String>();  
+		deque.add("Gautam");  
+		deque.add("Karan");  
+		deque.add("Ajay");  
+		//Traversing elements  
+		for (String str : deque) {  
+		System.out.println(str);  
+		}  
+		}  
+
+}
